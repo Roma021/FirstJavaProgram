1e2a0d340a75da434e2f31b509b3bc30c0f434ea
user.name Roma021 <GBTC441005UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-06-12 11:54:16 -0400

    GettingStoring

---
+++ src/GettingStoring.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class GettingStoring {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        String firstInitital = "P";
+        String lastName = "Sherman";
+        int houseNumber = 42;
+        String streetName = "Wallaby";
+        String streetType = "Way";
+        String city = "Sydney";
+
+        System.out.println(firstInitital + " " + lastName + " " + houseNumber + " " + streetName + "" + streetType + " " + city);
+
+
+    }
+}
