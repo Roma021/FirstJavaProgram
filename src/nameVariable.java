53ace9529d190d078a36dd0c2fb47c3eca4a4b5b
user.name Roma021 <GBTC441005UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-06-12 13:50:32 -0400

    MynameString

---
+++ src/nameVariable.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class nameVariable {
+    Scanner keyboard = new Scanner(System.in);
+    public static void main(String[] args) {
+        String myName = "David";
+        System.out.println(myName);
+    }
+}
