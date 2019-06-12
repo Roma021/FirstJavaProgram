26f1b2c34c1d7b98dc9e67185fe7d33463d4ed11
user.name Roma021 <GBTC441005UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-06-12 15:05:01 -0400

    Completion

--- src/Completion.java
+++ src/Completion.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class Completion {
+    Scanner keyboard = new Scanner(System.in);
+    public static void main(String[] args) {
+        String name = "Alex";
+        String breed = "Dobermann";
+        int age = 6;
+
+        System.out.println( "Greetings. What is your pet's name? " + name);
+        // name = keyboard.next();
+
+        System.out.println( "What kind of animal is " + name + "? " + breed);
+        System.out.println( "How old is " + name + "? " + age);
+
+        System.out.println( name + " is your " + breed + " and it is " + age );
+    }
+
+
 }
